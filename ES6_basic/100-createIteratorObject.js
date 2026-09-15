b9d82b27c1a9a48dcdbdc2610379548c1ef3946a
alaa2026ali <ali_alaa18@icloud.com>
2026-09-15 10:26:21 +0300

    Create 100-createIteratorObject.js

---
+++ ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,11 @@
+export default function createIteratorObject(report) {
+  const employees = [];
+
+  for (const department of Object.values(report.allEmployees)) {
+    for (const employee of department) {
+      employees.push(employee);
+    }
+  }
+
+  return employees[Symbol.iterator]();
+}
