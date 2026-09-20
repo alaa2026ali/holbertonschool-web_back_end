8a58a7e49563843dd880323e01d6f7f338b5ebda
alaa2026ali <ali_alaa18@icloud.com>
2026-09-20 09:55:53 +0300

    Create 8-clean_set.js

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+
+  return [...set]
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
+}
+
+export default cleanSet;
