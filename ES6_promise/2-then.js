a6b0246ba090007db569b626f24342e4951e24a8
alaa2026ali <ali_alaa18@icloud.com>
2026-09-21 08:22:12 +0300

    Create 2-then.js

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,25 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
+
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .then((response) => {
+      console.log('Got a response from the API');
+      return response;
+    })
+    .catch(() => new Error());
+}
+
